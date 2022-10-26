fd572e1e6b6389922c043803a86c247dfb93483e
Florian Matter <florianmatter@gmail.com>
2022-10-25 21:11:18 -0700

    release v0.0.1

--- src/biblatex2bibtex/__init__.py
+++ src/biblatex2bibtex/__init__.py
@@ -6,7 +6,7 @@ from pybtex.database.input import bibtex
 
 __author__ = "Florian Matter"
 __email__ = "florianmatter@gmail.com"
-__version__ = "0.0.1.dev"
+__version__ = "0.0.1"
 
 
 def convert(path, outpath):

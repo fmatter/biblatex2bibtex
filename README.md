022ad6b48e1e926c18264cfe9e4f0fac7919cf7c
Florian Matter <florianmatter@gmail.com>
2022-10-25 20:55:21 -0700

    new template

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
+# biblatex2bibtex
+
+![License](https://img.shields.io/github/license/fmatter/biblatex2bibtex)
+[![Linting](https://img.shields.io/github/workflow/status/fmatter/biblatex2bibtex/lint?label=linting)](https://github.com/fmatter/biblatex2bibtex/actions/workflows/lint.yml)
+[![PyPI](https://img.shields.io/pypi/v/biblatex2bibtex.svg)](https://pypi.org/project/biblatex2bibtex)
+![Versions](https://img.shields.io/pypi/pyversions/biblatex2bibtex)
+
+Convert biblatex files to bibtex.
+
+
 Usage:
 
 `biblatex2bibtex /path/to/biblatex_file.bib` (will create `/path/to/biblatex_file_out.bib`)
--- setup.py
+++ setup.py
@@ -1,34 +1,6 @@
-from setuptools import setup, find_packages
+#!/usr/bin/env python
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+import setuptools
 
-setup(
-    name="biblatex2bibtex",
-    version="0.0.1",
-    author="Florian Matter",
-    author_email="florianmatter@gmail.com",
-    description="Convert biblatex to bibtex",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/fmatter/biblatex2bibtex",
-    project_urls={
-        "Bug Tracker": "https://github.com/fmatter/biblatex2bibtex/issues",
-    },
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    install_requires=[
-        "pybtex==0.24.0",
-        "bibtexparser==1.2.0"
-    ],
-    entry_points={
-        "console_scripts": [
-            "biblatex2bibtex=biblatex2bibtex.__main__:main",
-        ],
-    },
-)
+if __name__ == "__main__":
+    setuptools.setup()
